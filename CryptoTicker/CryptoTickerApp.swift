cb0e9e57a52b5b285ae01514e4fe236d50a898a1
Luke Mao [SSW] <ErenJagerM@outlook.com>
2025-02-05 18:49:09 +1000

    Initial Commit

---
+++ CryptoTicker/CryptoTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTickerApp.swift
+//  CryptoTicker
+//
+//  Created by Luke Mao on 5/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
